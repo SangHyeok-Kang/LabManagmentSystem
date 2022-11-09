48db843464ca360c26adc6b258686ef5b58e6000
JunSeop-Song <ptj9654@naver.com>
2022-11-10 02:26:56 +0900

    [MOD] State & ReserList_model.java &ReserSearch_model.java

--- src/model/State/WaitAccessState.java
+++ src/model/State/WaitAccessState.java
@@ -6,11 +6,34 @@ import static model.DBConnection.dbconnection;
 
 public class WaitAccessState implements State {
     String reser_num;
+    String SQL;
     private Connection con = null;
     private Statement st =  null;
     private ResultSet rs = null;
+     private PreparedStatement pstmt = null;
     
     public void updateState(String reser_num){
+         String access = "e";
         
+        try {
+            //예약 상태가 대기 되었을 때 대기 상태로 바꿔주는 SQL문
+            SQL = "update reservation set access = ?" + "where reser_num = ?";
+            con = dbconnection.getConnection();
+            st = con.prepareStatement(SQL);
+            pstmt.setString(1,access);
+            pstmt.setString(2,reser_num);
+            st.executeUpdate(SQL);
+
+            rs.close();
+            st.close();
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }finally{
+            try{
+                if(pstmt !=null)pstmt.close();
+                if(con!=null)con.close();
+            } catch (SQLException e){ } 
+                
+        }
     }    
 }
